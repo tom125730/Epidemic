b8847a1a0ce5cf56ba7655dbe97b0c46555d50af
“chiupam” <chiupam@126.com>
2021-10-20 10:54:19 +0800

    修复

--- serverless/deploy.sh
+++ serverless/deploy.sh
@@ -11,8 +11,8 @@ cd ./serverless
 #删除lxml默认模块版本
 #echo "开始安装lxml"
 #sed -ie '/lxml==4.6.2/d' ./requirements.txt
-##解压lxml
-#sudo unzip lxml.zip
+#解压lxml
+sudo unzip lxml.zip
 
 #安装云函数需要的依赖库到severless文件夹
 echo "开始安装所需模块"

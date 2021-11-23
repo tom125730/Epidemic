480a3b141e6b56e550c40a6fbe5565516bccf027
chiupam <chiupam@126.com>
2021-11-23 10:36:32 +0800

    修改通知脚本

--- notify.py
+++ notify.py
@@ -3,24 +3,18 @@ from os.path import dirname, abspath, exists
 from json import load
 
 
-def push(title, content, template):
-    if notify and "XX" not in token:
-        url = 'http://www.pushplus.plus/send'
-        data = {'token': token, 'title': title, 'content': content, 'template': template}
-        print(requests.post(url, data=data).json())
-        
-
 def main():
-    global notify, token
+    url = 'http://www.pushplus.plus/send'
     with open("/data/config.json" if exists("/data/config.json") else f'{dirname(abspath(__file__))}/config.json', 'r', encoding='utf-8') as user_file:
         user_data = load(user_file)
     for user in user_data:
-        token = user['_token']
-        content = ""
-        push("今日签到情况说明", content, "txt")
+        if len(user['_token']) > 10:
+            name = user['_name']
+            content = ""
+            data = {'token': user['_token'], 'title': "标题", 'content': content, 'template': "txt"}
+            print(name, requests.post(url, data=data).json())
         
 
 if __name__ == '__main__':
-    notify, token = "", ""
     main()
     
